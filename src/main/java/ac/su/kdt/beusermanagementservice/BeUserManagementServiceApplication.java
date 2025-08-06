e4be383cbbf06b959035336de704e22b98e3700a
YeeunSeok <yes3641@naver.com>
2025-08-06 10:08:02 +0900

    Initial commit

---
+++ src/main/java/ac/su/kdt/beusermanagementservice/BeUserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.beusermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeUserManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeUserManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/su/kdt/beusermanagementservice/BeUserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.beusermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeUserManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
